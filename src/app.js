c73f06160a9c027d30895efa67eb6b8cef9b22ae
rough-g <rough00258707@gmail.com>
2021-06-26 13:20:59 +0900

    First Commit

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "normalize.css/normalize.css";
+import "./styles/style.scss";
+
+ReactDOM.render(<p>This is my builerplate</p>, document.getElementById("app"));
